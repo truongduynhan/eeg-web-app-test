f6ca54bd3838f4044d816cd9d90af5eeb140a2a0
Nhan Duy Truong <duynhan139@gmail.com>
2025-03-05 10:57:29 +0000

    plot eeg with plotly

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,16 @@ import streamlit as st
 from data_load import load_raw_data
 import os
 
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import chart_studio.plotly as py
+
+import mne  # If this line returns an error, uncomment the following line
+from mne.datasets import sample
+
 st.set_page_config(
     page_title="Visualize EEG",
     page_icon=":brain:",
@@ -14,8 +24,94 @@ st.title("Visualize EEG signals")
 fn = 'data/chb01_01.edf'
 if os.path.isfile(fn):
     signals, signal_headers, header = load_raw_data(fn)
-    print ('Main: Signal shape {}'.format(signals.shape))
-#     for signal_header in signal_headers:
-#         print (signal_header['label'], signal_header['sample_frequency'])
-#     print (header)
-# print ('Done!')
+    st.write ('Main: Signal shape {}'.format(signals.shape))
+    for signal_header in signal_headers:
+        print (signal_header['label'], signal_header['sample_frequency'])
+    # st.write (header)
+st.write ('Done!')
+
+
+
+date_range = pd.date_range(start="2023-01-01", end="2023-12-31", freq='M')
+data = date_range.month + np.random.normal(0, 1, size=len(date_range))
+data_green_line = np.linspace(start=20, stop=10, num=len(date_range)) + np.random.normal(0, 1, size=len(date_range))
+split_index = len(data) // 2
+
+trace_blue = go.Scatter(x=date_range[:split_index], y=data[:split_index], mode='lines', name='Blue Part', line=dict(color='blue'))
+trace_red = go.Scatter(x=date_range[split_index-1:], y=data[split_index-1:], mode='lines', name='Red Part', line=dict(color='red'))
+trace_green = go.Scatter(x=date_range, y=data_green_line, mode='lines', name='Green Line', line=dict(color='green'))
+
+data_combined = [trace_blue, trace_red, trace_green]
+
+layout = go.Layout(title='Combined Chart with Blue-Red Transition and Green Line',
+                   xaxis=dict(title='Date'),
+                   yaxis=dict(title='Data Value'),
+                   hovermode='closest')
+
+fig_combined = go.Figure(data=data_combined, layout=layout)
+
+st.plotly_chart(fig_combined)
+
+
+data_path = sample.data_path()
+
+raw_fname =  '{}/MEG/sample/sample_audvis_filt-0-40_raw.fif'.format(data_path)
+raw = mne.io.Raw(raw_fname, preload=False)
+print(raw)
+print(raw.ch_names[:5])
+
+data, times = raw[:, :10]
+print(data.shape)
+
+start, stop = raw.time_as_index([100, 115])  # 100 s to 115 s data segment
+data, times = raw[:306, start:stop]
+print(data.shape)
+print(times.shape)
+print(times.min(), times.max())
+
+picks = mne.pick_types(raw.info, meg='mag', exclude=[])
+data, times = raw[picks[:10], start:stop]
+
+
+# plt.plot(times, data.T)
+# plt.xlabel('time (s)')
+# plt.ylabel('MEG data (T)')
+
+# update = dict(layout=dict(showlegend=True), data=[dict(name=raw.info['ch_names'][p]) for p in picks[:10]])
+# py.iplot_mpl(plt.gcf(), update=update)
+
+from plotly import tools
+from plotly.graph_objs import Layout, YAxis, Scatter, Annotation, Annotations, Data, Figure, Marker, Font
+
+picks = mne.pick_types(raw.info, meg='grad', exclude=[])
+start, stop = raw.time_as_index([0, 10])
+
+n_channels = 20
+data, times = raw[picks[:n_channels], start:stop]
+ch_names = [raw.info['ch_names'][p] for p in picks[:n_channels]]
+
+step = 1. / n_channels
+kwargs = dict(domain=[1 - step, 1], showticklabels=False, zeroline=False, showgrid=False)
+
+# create objects for layout and traces
+layout = Layout(yaxis=YAxis(kwargs), showlegend=False)
+traces = [Scatter(x=times, y=data.T[:, 0])]
+
+# loop over the channels
+for ii in range(1, n_channels):
+        kwargs.update(domain=[1 - (ii + 1) * step, 1 - ii * step])
+        layout.update({'yaxis%d' % (ii + 1): YAxis(kwargs), 'showlegend': False})
+        traces.append(Scatter(x=times, y=data.T[:, ii], yaxis='y%d' % (ii + 1)))
+
+# add channel names using Annotations
+annotations = Annotations([Annotation(x=-0.06, y=0, xref='paper', yref='y%d' % (ii + 1),
+                                      text=ch_name, font=Font(size=9), showarrow=False)
+                          for ii, ch_name in enumerate(ch_names)])
+layout.update(annotations=annotations)
+
+# set the size of the figure and plot it
+layout.update(autosize=False, width=1000, height=600)
+fig = Figure(data=Data(traces), layout=layout)
+# py.iplot(fig, filename='shared xaxis')
+print ('plotly_chart')
+st.plotly_chart(fig)
